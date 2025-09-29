3b96eed0787b5be19bea81ae6018851a23b5b4b2
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2025-09-29 15:45:44 +0200

    make an object

--- Test.java
+++ Test.java
@@ -1,6 +1,12 @@
 public class Test{
+    String fname = "Rubel" ;
+    String lname = "Hosssain" ;
+    int age = 30 ;
+
     public static void main(String[] args)
-    {
+    {   
+        Test man = new Test() ;
+        System.out.println("Name is " + man.fname + " " + man.lname + " and " + man.age);
         System.out.println("Hello Java");
     }
 }
