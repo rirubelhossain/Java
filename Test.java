eea272e7d1fa0e4ffaf53d35c9f2aee21b98842b
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2025-09-29 15:57:03 +0200

    Set and Get Method

--- Test.java
+++ Test.java
@@ -1,12 +1,26 @@
 public class Test{
-    String fname = "Rubel" ;
-    String lname = "Hosssain" ;
-    int age = 30 ;
+    String fname ;
+    String lname ;
+    int age ;
+
+    public void getData(String a , String b , int age )
+    {
+        this.fname = a ;
+        this.lname = b ;
+        this.age = age ;
+
+    }
+    public void DisplayData()
+    {
+        System.out.println("Name is " + this.fname + " " + this.lname + " and age " + this.age);
+    }
 
     public static void main(String[] args)
     {   
         Test man = new Test() ;
-        System.out.println("Name is " + man.fname + " " + man.lname + " and " + man.age);
+        man.getData("Rubel","Hossain", 30);
+        man.DisplayData();
+        //System.out.println("Name is " + man.fname + " " + man.lname + " and " + man.age);
         System.out.println("Hello Java");
     }
 }
